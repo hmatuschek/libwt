91282444675e0812f9230a5da11d87152803378b
Hannes Matuschek <hmatuschek@gmail.com>
2015-11-20 13:35:46 +0100

    Even more cleanup.

--- lib/waveletconvolution.hh
+++ lib/waveletconvolution.hh
@@ -12,17 +12,37 @@ namespace wt {
  * of that function onto the space of wavelet transformed.
  * @bug Not implemented yet.
  * @ingroup analysis */
+template <class Scalar>
 class WaveletConvolution : public WaveletAnalysis
 {
 public:
   /** Constructs the convolution with the reproducting kernel of the specified wavelet pair. */
-  WaveletConvolution(const Wavelet &wavelet, const Eigen::VectorXd &Scales);
+  WaveletConvolution(const Wavelet &wavelet, const Eigen::VectorXd &Scales)
+    : WaveletAnalysis(wavelet, scales), _reprodKernel()
+  {
+    _init_convolution();
+  }
+
   /** Constructs the convolution with the reproducting kernel of the specified wavelet pair. */
-  WaveletConvolution(const Wavelet &wavelet, double *scales, size_t Nscales);
+  WaveletConvolution(const Wavelet &wavelet, double *scales, size_t Nscales)
+    : WaveletAnalysis(wavelet, scales, Nscales), _reprodKernel()
+  {
+    _init_convolution();
+  }
+
   /** Copy constructor. */
-  WaveletConvolution(const WaveletConvolution &other);
+  WaveletConvolution(const WaveletAnalysis &other)
+    : WaveletAnalysis(other), _reprodKernel()
+  {
+    _init_convolution();
+  }
+
   /** Destructor. */
-  virtual ~WaveletConvolution();
+  virtual ~WaveletConvolution() {
+    for (size_t i=0; i<_reprodKernel.size(); i++) {
+      delete _reprodKernel[i];
+    }
+  }
 
   template <class iDerived, class oDerived>
   void operator() (const Eigen::DenseBase<iDerived> &transformed, Eigen::DenseBase<oDerived> &out)
@@ -30,11 +50,24 @@ public:
   }
 
 protected:
-  void _init_convolution();
+  void _init_convolution() {
+    _reprodKernel.reserve(_scales.size());
+    // For every location of the reproducting kernel ...
+    for (int i=0; i<_scales.size(); i++) {
+      // Scale of the rep. kern.
+      double a = _scales[i];
+      // Determine the approx. scale range the rep. kernel is supported on.
+
+      // ...evaluate the kernel at every scale.
+      for (int j=0; j<_scales.size(); j++) {
+       /// @bug Implement.
+      }
+    }
+  }
 
 protected:
   /** The list of convolution filters applied for the convolution with the reproducing kernel. */
-  std::vector<Convolution<double> *> _reprodKernel;
+  std::vector<Convolution<Scalar> *> _reprodKernel;
 };
 
 }
