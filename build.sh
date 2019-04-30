885539b8359ca189ac674a4d85f11301aa940baa
Hannes Matuschek <hmatuschek@gmail.com>
2019-04-30 12:40:21 +0200

    Added build script.

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!env bash
+
+git pull
+rm -rf build
+mkdir build
+cd build
+cmake .. -DCMAKE_INSTALL_PREFIX=~/.local -DENABLE_PYTHON=OFF -DBUILD_UNITTEST=OFF -DENABLE_OPENMP=ON -DCMAKE_BUILD_TYPE=RELEASE
+make
+make install
+cd ..
+
